ae26e3917246bfe8437cfe4d37be707f108e496a
megha0707-aws <meghana.srinivas0707@gmail.com>
2026-05-22 19:17:06 +0530

    Switch to Gateway API for Azure ALB

--- main.tf
+++ main.tf
@@ -52,36 +52,68 @@ resource "kubernetes_service" "nginx" {
   }
 }
 
-resource "kubernetes_ingress_v1" "nginx_ingress" {
-  metadata {
-    name = "sample-web-ingress"
+resource "kubernetes_manifest" "gateway" {
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind       = "Gateway"
+    metadata = {
+      name      = "sample-gateway"
+      namespace = "default"
+      annotations = {
+        "alb.networking.azure.io/alb-id"         = "/subscriptions/91ea5a42-5e9b-4c0c-a766-ea2a2aaa3ace/resourceGroups/rg-alb-poc/providers/Microsoft.ServiceNetworking/trafficControllers/alb-poc"
+        "alb.networking.azure.io/frontend-name" = "public-frontend"
+      }
+    }
 
-    annotations = {
-  "alb.networking.azure.io/alb-id"         = "/subscriptions/91ea5a42-5e9b-4c0c-a766-ea2a2aaa3ace/resourceGroups/rg-alb-poc/providers/Microsoft.ServiceNetworking/trafficControllers/alb-poc"
-  "alb.networking.azure.io/frontend-name" = "public-frontend"
-}
+    spec = {
+      gatewayClassName = "azure-alb-external"
+
+      listeners = [
+        {
+          name     = "http"
+          port     = 80
+          protocol = "HTTP"
+        }
+      ]
+    }
   }
+}
 
-  spec {
-    ingress_class_name = "azure-alb-external"
+resource "kubernetes_manifest" "httproute" {
+  manifest = {
+    apiVersion = "gateway.networking.k8s.io/v1"
+    kind       = "HTTPRoute"
+    metadata = {
+      name      = "sample-route"
+      namespace = "default"
+    }
 
-    rule {
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
+    spec = {
+      parentRefs = [
+        {
+          name = "sample-gateway"
+        }
+      ]
 
-          backend {
-            service {
-              name = kubernetes_service.nginx.metadata[0].name
-
-              port {
-                number = 80
+      rules = [
+        {
+          matches = [
+            {
+              path = {
+                type  = "PathPrefix"
+                value = "/"
               }
             }
-          }
+          ]
+
+          backendRefs = [
+            {
+              name = "sample-web-service"
+              port = 80
+            }
+          ]
         }
-      }
+      ]
     }
   }
 }
